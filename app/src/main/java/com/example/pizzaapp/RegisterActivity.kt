009a595886782e66fdd6a70c4efbe8838f2c270d
amikom <rioraditya37@gmail.com>
2023-11-22 16:53:57 +0700

    rveis pt2 22/11

--- app/src/main/java/com/example/pizzaapp/RegisterActivity.kt
+++ app/src/main/java/com/example/pizzaapp/RegisterActivity.kt
@@ -1,9 +1,11 @@
 package com.example.pizzaapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,6 +24,28 @@ btnRegister.setOnClickListener {
     //object class databaseHelper
     val databaseHelper = DatabaseHelper(this)
     //declare data
+    val email:String = txtEmail.text.toString().trim()
+    val name:String = txtName.text.toString().trim()
+    val level:String = txtLevel.text.toString().trim()
+    val password:String = txtPassword.text.toString().trim()
+
+    //check data -> email sudah terdaftar atau beloooom
+    val data:String = databaseHelper.checkData(email)
+    //jika belom daftar
+    if(data == ""){ //data => null
+        //insert data
+        databaseHelper.addAccount(
+            email,name, level, password)
+
+        //show LoginActivity
+        val intentLogin = Intent(this@RegisterActivity,
+            LoginActivity::class.java)
+        startActivity(intentLogin)
+    }else{
+        //jika email telah terdaftar
+        Toast.makeText(this@RegisterActivity,"Register Failed." +
+            "Your email alredy registered", Toast.LENGTH_SHORT).show()
+    }
 }
     }
 }
